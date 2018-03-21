a3cfd86fc08d885ccda066b7f3dac9cc2fc033c6
Eric Potter <pottereric@hotmail.com>
2018-03-21 11:46:41 -0400

    Update the version number.

--- src/FognitiveServices/AssemblyInfo.fs
+++ src/FognitiveServices/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FognitiveServices")>]
 [<assembly: AssemblyProductAttribute("FognitiveServices")>]
 [<assembly: AssemblyDescriptionAttribute("An F# wrapper around the Azure Cognitive Services APIs.")>]
-[<assembly: AssemblyVersionAttribute("1.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FognitiveServices"
     let [<Literal>] AssemblyProduct = "FognitiveServices"
     let [<Literal>] AssemblyDescription = "An F# wrapper around the Azure Cognitive Services APIs."
-    let [<Literal>] AssemblyVersion = "1.0.1"
-    let [<Literal>] AssemblyFileVersion = "1.0.1"
+    let [<Literal>] AssemblyVersion = "0.0.1"
+    let [<Literal>] AssemblyFileVersion = "0.0.1"
     let [<Literal>] AssemblyConfiguration = "Release"
